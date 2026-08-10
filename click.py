cd76f70a1cd06464127cd873bb7b798531e41bd1
RustDesk <cryon1914@gmail.com>
2026-08-10 10:14:32 +0100

    Update click.py

--- click.py
+++ click.py
@@ -14,8 +14,8 @@ def generate_random_string(length=9):
 def get_proxy_config():
     """Generate proxy configuration with random session string"""
     random_session = generate_random_string(9)
-    username = f"sZUGRbnc-session-{random_session}"
-    password = "Wcukyw4N"
+    username = f"Pd3aXzKd-session-{random_session}"
+    password = "SWJ3dr6a"
     
     return {
         "server": "http://gateway.aluvia.io:8080",
